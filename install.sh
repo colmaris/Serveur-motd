c1d432ecc0ef1ffa5fde13f5db4f8964204eca29
Draconis <olivier@delort.email>
2019-02-19 13:43:31 +0100

    initial commit

--- install.sh
+++ install.sh
@@ -10,8 +10,25 @@ then
 fi
 
 # install the motd
+echo "Installing motd..."
 cp motd /usr/local/bin/
-chmod +x /usr/local/bin/motd
+cp lxc /usr/local/bin/
+
 
 #install the maintenance file
 cp motd-maint /etc/motd-maint
+
+#Disable the motd in SSH Server
+sed -i 's/#PrintMotd yes/PrintMotd no/g' /etc/ssh/sshd_config
+#Enable motd for bash user
+echo "/usr/local/bin/motd" > /etc/profile.d/motd.sh
+chmod +x /etc/profile.d/motd.sh
+# for zsh users
+if [ -f "/usr/bin/zsh" ];then
+echo "/usr/local/bin/motd" >> ~/.zprofile;
+fi
+#disable the system motd
+echo "" > /etc/motd
+chmod +x /usr/local/bin/motd
+chmod +x /usr/local/bin/lxc
+exit 0
